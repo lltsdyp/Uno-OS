ba5874f10a13ef7aa92cc5204d1a0105cc42b9c0
psycho-round <10235101500@stu.ecnu.edu.cn>
2024-11-10 23:05:30 +0630

    任务3

--- kernel/mem/mmap.c
+++ kernel/mem/mmap.c
@@ -4,6 +4,7 @@
 #include "mem/pmem.h"
 #include "mem/vmem.h"
 #include "mem/mmap.h"
+#include "memlayout.h"
 
 // 包装 mmap_region_t 用于仓库组织
 typedef struct mmap_region_node {
@@ -22,6 +23,16 @@ static spinlock_t list_lk;
 // 初始化上述三个数据结构
 void mmap_init()
 {
+    spinlock_init(&list_lk, "lk");
+    list_head = list_mmap_region_node;
+    for(int i = 0; i < N_MMAP; ++i)
+    {
+        list_mmap_region_node[i].mmap.begin = MMAP_BEGIN;
+        list_mmap_region_node[i].mmap.npages = 0;
+
+        // 还要判断是否是最后一个
+        list_mmap_region_node[i].next = (i == N_MMAP) ? NULL : &list_mmap_region_node[i+1];
+    }
 }
 
 // 从仓库申请一个 mmap_region_t
@@ -29,13 +40,29 @@ void mmap_init()
 // 注意: list_head 保留, 不会被申请出去
 mmap_region_t* mmap_region_alloc()
 {
+    spinlock_acquire(&list_lk);
+    mmap_region_node_t* region = list_head->next;
+    
+    assert(region == NULL, "mmap_region_alloc failed");
 
+    list_head->next = region->next;
+    spinlock_release(&list_lk);
+
+    return &region->mmap;
 }
 
 // 向仓库归还一个 mmap_region_t
 void mmap_region_free(mmap_region_t* mmap)
 {
+    spinlock_acquire(&list_lk);
+    uint64 begin = (uint64)(&list_mmap_region_node->mmap);
 
+    // 头插法
+    int idx = ((uint64)mmap - begin) / sizeof(list_mmap_region_node[0]);
+    list_mmap_region_node[idx].next = list_head->next;
+    list_head->next = &list_mmap_region_node[idx];
+
+    spinlock_release(&list_lk);
 }
 
 // 输出仓库里可用的 mmap_region_node_t
