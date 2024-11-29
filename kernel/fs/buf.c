2a8ed2d69fd4f28568e39bf7f3105c7dc2438ece
lltsdyp <jizimo0430@outlook.com>
2024-11-29 16:05:18 +0000

    在buf_release中寻找buf块

--- kernel/fs/buf.c
+++ kernel/fs/buf.c
@@ -119,8 +119,15 @@ void buf_release(buf_t* buf)
     // 当前是最后一个使用这个buf块的
     if(buf->buf_ref==0)
     {
+        // 寻找对应的位置
+        buf_node_t *buf_node=head_buf;
+        spinlock_acquire(&lk_buf_cache);
+        while(&(buf_node->buf)!=buf)
+            buf_node=buf_node->next;
+        assert(buf_node!=&head_buf, "buf_release: buf not found");
         // 尾插
         insert_head(&(buf->node), 0);   
+        spinlock_release(&lk_buf_cache);
     }
     spinlock_release(&lk_buf_cache);
 }
