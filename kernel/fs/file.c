956371a5ec4b4721682e0b5be7ba64b2a1cd97ad
lltsdyp <jizimo0430@outlook.com>
2024-12-17 05:10:44 +0000

    添加file.c

--- kernel/fs/file.c
+++ kernel/fs/file.c
@@ -1,34 +1,101 @@
-#include "riscv.h"
+#include "fs/fs.h"
+#include "fs/buf.h"
+#include "fs/dir.h"
+#include "fs/bitmap.h"
+#include "fs/inode.h"
 #include "fs/file.h"
-#include "lib/lock.h"
-#include "proc/proc.h"
-#include 
+#include "mem/vmem.h"
+#include "proc/cpu.h"
+#include "lib/print.h"
 
-dev_t devs[N_DEV];
+// 设备列表(读写接口)
+dev_t devlist[N_DEV];
 
-file_t files[N_FILE]
-spinlock_t flock;
+// ftable + 保护它的锁
+#define N_FILE 32
+file_t ftable[N_FILE];
+spinlock_t lk_ftable;
 
+// ftable初始化 + devlist初始化
 void file_init()
 {
-    spinlock_init(&flock, "flock");
+
 }
 
-file_t *file_alloc()
+// alloc file_t in ftable
+// 失败则panic
+file_t* file_alloc()
 {
-    spinlock_acquire(&flock);
-    for (int i = 0; i < N_FILE; i++) {
-        if (files[i].ref == 0) {
-            files[i].ref=1;
-            spinlock_release(&flock);
-            return &files[i];
-        }
-    }
-    spinlock_release(&flock);
-    return NULL;
+
 }
 
+// 创建设备文件(供proczero创建console)
 file_t* file_create_dev(char* path, uint16 major, uint16 minor)
 {
 
+}
+
+// 打开一个文件
+file_t* file_open(char* path, uint32 open_mode)
+{
+
+}
+
+// 释放一个file
+void file_close(file_t* file)
+{
+
+}
+
+// 文件内容读取
+// 返回读取到的字节数
+uint32 file_read(file_t* file, uint32 len, uint64 dst, bool user)
+{
+
+}
+
+// 文件内容写入
+// 返回写入的字节数
+uint32 file_write(file_t* file, uint32 len, uint64 src, bool user)
+{
+
+}
+
+// flags 可能取值
+#define LSEEK_SET 0  // file->offset = offset
+#define LSEEK_ADD 1  // file->offset += offset
+#define LSEEK_SUB 2  // file->offset -= offset
+
+// 修改file->offset (只针对FD_FILE类型的文件)
+uint32 file_lseek(file_t* file, uint32 offset, int flags)
+{
+
+}
+
+// file->ref++ with lock
+file_t* file_dup(file_t* file)
+{
+    spinlock_acquire(&lk_ftable);
+    assert(file->ref > 0, "file_dup: ref");
+    file->ref++;
+    spinlock_release(&lk_ftable);
+    return file;
+}
+
+// 获取文件状态
+int file_stat(file_t* file, uint64 addr)
+{
+    file_state_t state;
+    if(file->type == FD_FILE || file->type == FD_DIR)
+    {
+        inode_lock(file->ip);
+        state.type = file->ip->type;
+        state.inode_num = file->ip->inode_num;
+        state.nlink = file->ip->nlink;
+        state.size = file->ip->size;
+        inode_unlock(file->ip);
+
+        uvm_copyout(myproc()->pgtbl, addr, (uint64)&state, sizeof(file_state_t));
+    }
+    return -1;
 }
